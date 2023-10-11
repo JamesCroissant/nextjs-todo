7ab6fd09bc2d53e9292260d0738a7838c6a54cf0
Yu Hamada <yuham1014@gmail.com>
2023-10-11 13:37:37 -0700

    update Todo component

--- nextjs-todo/app/components/TodoList.tsx
+++ nextjs-todo/app/components/TodoList.tsx
@@ -1,17 +1,20 @@
+import { Task } from '@/types';
 import React from 'react'
+import Todo from './Todo';
 
-const TodoList = () => {
+interface TodoListProps {
+  todos: Task[];
+}
+
+
+const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul className="space-y-3">
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>walk</span>
-        <div>
-          <button className="text-green-500">edit</button>
-          <button className="text-red-500">delete</button>
-        </div>
-      </li>
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
     </ul>
   )
 }
 
-export default TodoList
+export default TodoList;
